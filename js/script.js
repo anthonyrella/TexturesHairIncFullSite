c120c1941d68920093a24839c4e1287d410a2600
Anthony Rella <anthonyrella@outlook.com>
2019-11-03 21:16:27 +0200

    Small edit to javascript file

--- js/script.js
+++ js/script.js
@@ -44,23 +44,16 @@ $(document).ready(function () {
 
     function check_if_in_view() {
         var window_height = $window.height();
-        console.log(window_height)
         var window_top_position = $window.scrollTop();
-        console.log(window_top_position)
 
         var window_bottom_position = (window_top_position + window_height);
-        console.log(window_bottom_position)
-
         $.each($animation_elements, function() {
           var $element = $(this);
           var element_height = $element.outerHeight();
-          console.log(element_height)
 
           var element_top_position = $element.offset().top;
-          console.log(element_top_position)
 
           var element_bottom_position = (element_top_position + element_height);
-          console.log(element_bottom_position)
 
           //check to see if this current container is within viewport
           if ((element_bottom_position >= window_top_position) &&
